f05bb6e9818eb010758fe43d610e0faac0706146
favouritesir <102854760+favouritesir@users.noreply.github.com>
2024-05-23 11:07:53 +0600

    learn os module in nodejs

---
+++ core-module/os.js
@@ -0,0 +1,13 @@
+// need to improve app performance ==>
+const os=require("os");
+
+console.log(os.platform());//win32
+console.log(os.type());//Windows_NT
+console.log(os.machine());//x86_64
+console.log(os.homedir());//C:\Users\LAPTOP CARE
+console.log(os.hostname());//favouritesir
+
+console.log(os.freemem());//3429470208 ==> ram size in byte
+console.log(os.totalmem());//8450351104 ==> ram size in byte
+
+
