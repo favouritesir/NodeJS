c140e386e655ed89da3eff17c334e1f1950c0a59
favouritesir <102854760+favouritesir@users.noreply.github.com>
2024-05-23 12:22:45 +0600

    work with fs learn create folder or folders and files and update files

---
+++ core-module/fs.js
@@ -0,0 +1,27 @@
+const fs=require("fs");
+if(!fs.existsSync("./files_by_fs_module")){
+    try{
+        fs.mkdirSync("./files_by_fs_module")
+    }catch(err){
+        console.log(err);
+    }
+}
+
+// create 1st file on different folder;
+fs.writeFileSync("./files_by_fs_module/1st.txt","ma-shaAllah 1st file is created.\n");
+console.log("1st: "+fs.readFileSync("./files_by_fs_module/1st.txt"));//read 1st file
+
+// create 2nd file on different folder;
+fs.writeFileSync("./files_by_fs_module/2nd.txt","ma-shaAllah 2nd file is created.\n");
+console.log("2nd: "+fs.readFileSync("./files_by_fs_module/2nd.txt"));
+// replace 2nd file
+fs.writeFileSync("./files_by_fs_module/2nd.txt","ma-shaAllah 2nd file is replaced and update");
+console.log("2nd replace: "+fs.readFileSync("./files_by_fs_module/2nd.txt"));
+
+
+// create 2nd file on different folder;
+fs.writeFileSync("./files_by_fs_module/3rd.txt","ma-shaAllah 3rd file is created.\n");
+console.log("3rd: "+fs.readFileSync("./files_by_fs_module/3rd.txt"));
+// append 2nd file
+fs.appendFileSync("./files_by_fs_module/3rd.txt","ma-shaAllah 3rd file is append and update");
+console.log("3rd append: "+fs.readFileSync("./files_by_fs_module/3rd.txt"));
--- core-module/os.js
+++ core-module/os.js
@@ -4,10 +4,41 @@ const os=require("os");
 console.log(os.platform());//win32
 console.log(os.type());//Windows_NT
 console.log(os.machine());//x86_64
+console.log(os.arch());// x64
 console.log(os.homedir());//C:\Users\LAPTOP CARE
 console.log(os.hostname());//favouritesir
 
 console.log(os.freemem());//3429470208 ==> ram size in byte
 console.log(os.totalmem());//8450351104 ==> ram size in byte
 
+// other important 
+console.log(os.cpus());// give cpus core and other info
+// [
+//     {
+//       model: 'Intel(R) Core(TM) i3-4130 CPU @ 3.40GHz',
+//       speed: 3392,
+//       times: {
+//         user: 1906484,
+//         nice: 0,
+//         sys: 1375093,
+//         idle: 13846343,
+//         irq: 82140
+//       }
+//     },
+//     {
+//       model: 'Intel(R) Core(TM) i3-4130 CPU @ 3.40GHz',
+//       speed: 3392,
+//       times: { user: 1964984, nice: 0, sys: 971859, idle: 14190937, irq: 19156 }
+//     },
+//     {
+//       model: 'Intel(R) Core(TM) i3-4130 CPU @ 3.40GHz',
+//       speed: 3392,
+//       times: { user: 2218500, nice: 0, sys: 972265, idle: 13937015, irq: 15640 }
+//     },
+//     {
+//       model: 'Intel(R) Core(TM) i3-4130 CPU @ 3.40GHz',
+//       speed: 3392,
+//       times: { user: 2224703, nice: 0, sys: 837125, idle: 14065953, irq: 18859 }
+//     }
+//   ]
 
