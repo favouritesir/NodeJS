b8931145af640a138b3167bbfca323d77f5eee65
favouritesir <102854760+favouritesir@users.noreply.github.com>
2024-05-23 10:22:03 +0600

    add module.js for understand the nodejs moduler concept

---
+++ basic/module.js
@@ -0,0 +1,13 @@
+// in node js every file is modiul. let me show the system
+// #########################################################
+// here is global object
+// (function (exports,require,module,__filename,__dirname) {
+// code on our js file
+
+console.log(global);
+console.log(module);
+console.log(__filename);
+console.log(__dirname);
+
+// return module.export
+// }());
